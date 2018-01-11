0206f3ff0eaf19a57961ca5ec260a6edd2d20d4d
Yulia Drozdova <yuliannka@gmail.com>
2018-01-11 13:49:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = {};
 playlist.song = 'Katyusha';
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle;
+  playlist[artistName] = songTitle;
   return playlist
 }
